158fc0ef33bed423a78eaa3d176c8c62bf725cb1
Brittnie <brittniesexton95@gmail.com>
2022-01-26 16:24:40 -0600

    Wrote out the UML for the three classes Node, Iterator, and List

---
+++ LinkedList/List.h
@@ -0,0 +1,94 @@
+#pragma once
+#include "Iterator.h"
+#include "Node.h"
+
+template<typename T>
+class List
+{
+public:
+	List();
+	List(const List<T>& other);
+	~List();
+
+	/// <summary>
+	/// Deletes all nodes in the list
+	/// </summary>
+	void destroy();
+
+	/// <returns>Retruns an iterator pointing to the first node in the list</returns>
+	Iterator<T> begin() const;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns>Returns the nex item after the last node in the list</returns>
+	Iterator<T> end() const;
+
+	/// <summary>
+	/// Checks to see if the given item is in the list
+	/// </summary>
+	/// <param name="object">The item to find in the list</param>
+	/// <returns>The item in the list</returns>
+	bool contains(const T object) const;
+
+	/// <summary>
+	/// Adds a new node to the beginning of the list
+	/// </summary>
+	/// <param name="object">The new node to add at the beginning</param>
+	void pushFront(const T object) const;
+
+	/// <summary>
+	/// Adds a new node to the end of the list
+	/// </summary>
+	/// <param name="value">The new node to add at the end</param>
+	void pushBack(const T& value);
+
+	/// <summary>
+	/// Adds a new node at the given index
+	/// </summary>
+	/// <param name="value">The new node to insert</param>
+	/// <param name="index">Where the node will be inserted</param>
+	/// <returns></returns>
+	bool insert(const T& value, int index);
+
+	/// <summary>
+	/// Remove all nodes with the given value
+	/// </summary>
+	/// <param name="value">The value the node, that needs to be removed, has</param>
+	/// <returns></returns>
+	bool remove(const T& value);
+
+	/// <summary>
+	/// Prints the values for all the nodes
+	/// </summary>
+	void print() const;
+
+	/// <summary>
+	/// Set the default values for the first node pointer, 
+	/// the last node pointer, and the node count
+	/// </summary>
+	void initialize();
+
+	/// <returns>Whether or not the list has any nodes in it</returns>
+	bool isEmpty() const;
+
+	/// <summary>
+	/// Sets the given iterator to point to a node at the given index
+	/// </summary>
+	/// <param name="iter">The iterator</param>
+	/// <param name="index">The index the iterator will point at</param>
+	/// <returns></returns>
+	bool getData(Iterator<T>& iter, int index);
+
+	/// <returns>The amount of nodes in the list</returns>
+	int getLength() const;
+
+	const List<T>& operator= (constList<T>& otherList);
+
+	void sort();
+
+private:
+	Node<T>* m_first;
+	Node<T>* m_last;
+	int m_nodeCount;
+};
---
+++ LinkedList/Node.h
@@ -0,0 +1,12 @@
+#pragma once
+
+template<typename T>
+class Node
+{
+public:
+	Node();
+	Node(T value);
+	Node<T>* next;
+	Node<T>* previous;
+	T data;
+};
