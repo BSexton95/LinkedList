3c0eff332ae79353c695f839feef778df3f57d6f
Brittnie <brittniesexton95@gmail.com>
2022-01-28 11:39:18 -0600

    Completed insert and remove function

--- LinkedList/List.h
+++ LinkedList/List.h
@@ -35,7 +35,7 @@ public:
 	/// Adds a new node to the beginning of the list
 	/// </summary>
 	/// <param name="object">The new node to add at the beginning</param>
-	void pushFront(const T object) const;
+	void pushFront(const T& object) const;
 
 	/// <summary>
 	/// Adds a new node to the end of the list
@@ -81,7 +81,7 @@ public:
 	bool getData(Iterator<T>& iter, int index);
 
 	/// <returns>The amount of nodes in the list</returns>
-	int getLength() const;
+	int getLength() const { return m_nodeCount; }
 
 	const List<T>& operator= (constList<T>& otherList);
 
@@ -108,29 +108,145 @@ List<T>::List(const List<T>& other)
 }
 
 template<typename T>
-inline void List<T>::destroy()
+void List<T>::destroy()
 {
 
 }
 
 template<typename T>
-inline Iterator<T> List<T>::begin() const
+Iterator<T> List<T>::begin() const
 {
-	return m_first;
+	for (Iterator<T> iter = begin(); iter != end(); iter++)
+	{
+		return iter;
+	}
+	return ;
 }
 
 template<typename T>
-inline Iterator<T> List<T>::end() const
+Iterator<T> List<T>::end() const
 {
 	return m_last;
 }
 
 template<typename T>
-inline bool List<T>::contains(const T object) const
+bool List<T>::contains(const T object) const
 {
 	for (int i = 0; i < m_nodeCount; i++)
 	{
 		if ()
 	}
 	return false;
+}
+
+template<typename T>
+void List<T>::pushFront(const T& object) const
+{
+	Node<T> nodeInserted = new Node<T>(object);
+	nodeInserted.data = object;
+	nodeInserted.next = m_first;
+	m_first->previous = nodeInserted;
+}
+
+template<typename T>
+void List<T>::pushBack(const T& value)
+{
+	Node<T> nodeInserted = new Node<T>(value);
+	nodeInserted.data = value;
+	nodeInserted.next = nullptr;
+	nodeInserted.previous = m_last->previous;
+	m_last = nodeInserted;
+}
+
+template<typename T>
+bool List<T>::insert(const T& value, int index)
+{
+	//Iterate through linked list
+	for (Iterator<T> iter = begin(); iter == index; iter++)
+	{
+		//Create new node
+		Node<T> newNode = new Node<T>;
+		newNode.data = value;
+		newNode.next = iter.m_current;
+
+		//if the current node is null...
+		if (iter.m_current != nullptr)
+		{
+			//...set the new nodes previous to be the current nodes previous
+			newNode.previous = iter.m_current->previous;
+
+			//if the current nodes previous is null...
+			if (iter.m_current->previous != nullptr)
+			{
+				//...set the current nodes previouses next to be the new node
+				iter.m_current->previous->next = newNode;
+			}
+
+			//set the current nodes previous to be the new node
+			iter.m_current->previous = newNode;
+		}
+
+		if (m_first == iter.m_current)
+			m_first = newNode;
+
+		if (m_last->next == nullptr)
+			m_last = newNode;
+
+	}
+}
+
+template<typename T>
+bool List<T>::remove(const T& value)
+{
+	for (Iterator<T> iter = begin(); iter != value.next; iter++)
+	{
+		iter.m_current->next = value.next;
+		value.next.previous = iter.m_current;
+	}
+
+	return false;
+}
+
+template<typename T>
+void List<T>::print() const
+{
+	for (Iterator<T> iter = begin(); iter != end(); iter++)
+		std::cout << iter << std::endl;
+}
+
+template<typename T>
+void List<T>::initialize()
+{
+}
+
+template<typename T>
+bool List<T>::isEmpty() const
+{
+	return false;
+}
+
+template<typename T>
+bool List<T>::getData(Iterator<T>& iter, int index)
+{
+	return false;
+}
+
+template<typename T>
+const List<T>& List<T>::operator=(const List<T>& otherList)
+{
+	// TODO: insert return statement here
+}
+
+template<typename T>
+void List<T>::sort()
+{
+	for (int i = 1; i < m_nodeCount; i++)
+	{
+		for (int j = 0; j < m_nodeCount; j++)
+		{
+			T tempslot = begin();
+
+			if ()
+		}
+	}
 }
