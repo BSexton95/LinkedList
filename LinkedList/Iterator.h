8c21dbdf0fcb1db414cda52ce4d71a8669b53a3c
Brittnie <brittniesexton95@gmail.com>
2022-01-31 09:37:41 -0600

    Corrected the iterator class

--- LinkedList/Iterator.h
+++ LinkedList/Iterator.h
@@ -32,25 +32,35 @@ Iterator<T>::Iterator(Node<T>* node)
 template<typename T>
 Iterator<T> Iterator<T>::operator++()
 {
-	return m_current++;
+	if (m_current->next == nullptr)
+		return m_current;
+	else
+		return m_current->next;
 }
 
 template<typename T>
 Iterator<T> Iterator<T>::operator--()
 {
-	return m_current--;
+	if (m_current->previous = nullptr)
+		return m_current;
+	else
+		return m_current->previous;
 }
 
 template<typename T>
 const bool Iterator<T>::operator==(const Iterator<T>& iter)
 {
-	return m_current = iter;
+	return m_current->data == iter.m_current->data &&
+		m_current->next == iter.m_current->next &&
+		m_current->previous == iter.m_current;
 }
 
 template<typename T>
 inline const bool Iterator<T>::operator!=(const Iterator<T>& iter)
 {
-	return m_current != iter;
+	return m_current->data != iter.m_current->data &&
+		m_current->next != iter.m_current->next &&
+		m_current->previous != iter.m_current->next;
 }
 
 template<typename T>
