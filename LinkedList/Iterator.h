6fc81563b10090197c5a6df5ebb1359c61ee16c0
Brittnie <brittniesexton95@gmail.com>
2022-02-17 19:51:41 -0600

    Added comments

--- LinkedList/Iterator.h
+++ LinkedList/Iterator.h
@@ -29,15 +29,15 @@ public:
 	Iterator<T> operator--();
 
 	/// <param name="iter">The iterator to compare</param>
-	/// <returns>If the current node is equal to the iterator</returns>
+	/// <returns>If two lists are equal</returns>
 	const bool operator ==(const Iterator<T>& iter);
 
 	/// <param name="iter">The iterator to compare</param>
-	/// <returns>If the current node is not equal to the itertor</returns>
+	/// <returns>two list are not equal</returns>
 	const bool operator != (const Iterator<T>& iter);
 
 	/// <summary>
-	/// 
+	/// Returns returns the data the node is pointing at
 	/// </summary>
 	/// <returns></returns>
 	T operator*();
@@ -62,23 +62,29 @@ template<typename T>
 Iterator<T> Iterator<T>::operator++()
 {
 	if (m_current->next == nullptr)
-		return m_current;
+		return Iterator<T>(m_current);
 	else
-		return m_current->next;
+		return Iterator<T>(m_current->next);
 }
 
 template<typename T>
 Iterator<T> Iterator<T>::operator--()
 {
 	if (m_current->previous = nullptr)
-		return m_current;
+		return Iterator<T>(m_current);
 	else
-		return m_current->previous;
+		return Iterator<T>(m_current->previous);
 }
 
 template<typename T>
 const bool Iterator<T>::operator==(const Iterator<T>& iter)
 {
+	//If the iteraters current node is null...
+	if (iter.m_current == nullptr)
+		//...exit the function and return false
+		return false;
+
+	//Returns if the current nodes data, next node, and previous node are equal
 	return m_current->data == iter.m_current->data &&
 		m_current->next == iter.m_current->next &&
 		m_current->previous == iter.m_current;
@@ -87,6 +93,12 @@ const bool Iterator<T>::operator==(const Iterator<T>& iter)
 template<typename T>
 inline const bool Iterator<T>::operator!=(const Iterator<T>& iter)
 {
+	//If the iteraters current is null...
+	if (iter.m_current == nullptr)
+		//...exit function and return false
+		return false;
+
+	//Returns if the current nodes data, next node, and previous node are not equal
 	return m_current->data != iter.m_current->data &&
 		m_current->next != iter.m_current->next &&
 		m_current->previous != iter.m_current->next;
@@ -95,5 +107,5 @@ inline const bool Iterator<T>::operator!=(const Iterator<T>& iter)
 template<typename T>
 inline T Iterator<T>::operator*()
 {
-	return T();
+	return m_current->data;
 }
